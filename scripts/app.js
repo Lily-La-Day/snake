386aec3d35cc85630cd966e90dee0df151bf1b8c
Lily Keeling <lily.keeling@gmail.com>
2019-06-08 07:56:27 +0100

    grid done

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-console.log('good morning')
+const width = 100
+const squares = []
+let playerIndex = 0
+
+
+const init = () => {
+
+  for(let i = 0; i < width * width; i ++ ) {
+  const grid = document.querySelector('.grid')
+console.log(grid)
+     const square = document.createElement('div')
+     grid.append(square)
+     square.classList.add('grid-item')
+     square.dataset.index = i
+     // square.innerHTML = i
+
+
+
+
+  }
+}
+
+window.addEventListener('DOMContentLoaded', init)
